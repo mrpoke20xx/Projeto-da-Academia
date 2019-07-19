28a2d58de8c9edcafc8e059d96b767c311200027
Murilovc <51269932+Murilovc@users.noreply.github.com>
2019-07-19 07:20:39 -0500

    É hoje

--- src/br/Aca/Gui/TreinoCadastro.java
+++ src/br/Aca/Gui/TreinoCadastro.java
@@ -1,92 +1,102 @@
 package br.Aca.Gui;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
-import java.sql.ResultSet;
+import javax.imageio.ImageIO;
+import javax.swing.*; 					//importando classes do Swing
 
-import br.Aca.DB.*;
-import br.Aca.Entity.*;
+import br.Aca.DB.Conexao;
+import br.Aca.Entity.Treino;
+import br.Aca.Logic.TreinoLogic;
 import br.Aca.Exception.*;
-import br.Aca.Logic.*;
 
+import java.awt.*; 						//importando classes do AWT
+import java.awt.event.*; 				//importando classes de EVENTOS do AWT
+import java.io.File;
+import java.io.IOException;
+import java.sql.*;						//importando classes do JDBC
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class TrainerCadastro extends JFrame{
-	
+class TreinoCadastro extends JFrame {
+
 	private final int INCLUSAO = 0;
 	private final int EDICAO = 1;
 	private final int EXCLUSAO = 2;
-	
-	private int acao;
-	
-	private TrainerConsulta pai;
+
+	private int acao, numeroDeCentros;
+	private String[] idCentros;
+
+	private TreinoConsulta pai;
 	private Conexao cnx;
 	private ResultSet rs;
-	private TrainerLogic tl;
-	
+	private TreinoLogic tl;
+
 	private JPanel pControles, pOperacoes, pRotulos, pCampos;
-	private JComboBox cbox;
-	private JTextField tfCodigo, tfNome, tfDataNasc, tfSexo, tfAcademia;
+	private JLabel lblImagem;
+	private JComboBox cmbCentro;
+	private JTextField tfCodigo, tfVencimento, tfCliente, tfExercicio;
 	private JButton btConfirmar, btCancelar;
+
 	private SimpleDateFormat form = new SimpleDateFormat("dd/MM/yyyy");
 	
 	AcaoConfirmar actConfirmar = new AcaoConfirmar();
 	AcaoCancelar actCancelar = new AcaoCancelar();
-	
-	static final String imagesPath = new String("images/");
-	
-	TrainerCadastro(JFrame framePai, Conexao essacnx) { //mÃ©todo construtor
-		super(""); //Chamando o construtor da classe mae
-		setSize(800,600); //DimensÃµes da janela
+
+	static final String imagesPath = new String("images/");	
+
+	TreinoCadastro(JFrame framePai, Conexao conexao){ // metodo construtor
+		super(""); // chamando construtor da classe mae
+		setSize(1400, 600);				// definindo dimensoes da janela		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		
-		pai = (TrainerConsulta)framePai;
-		cnx = essacnx;
-		
-		tl = new TrainerLogic(cnx);
-		
-		pRotulos = new JPanel(new GridLayout(7,1,5,5));
-		pRotulos.add(new JLabel("Codigo: "));
-		pRotulos.add(new JLabel("Nome: "));
-		pRotulos.add(new JLabel("Data de Nascimento: "));
-		pRotulos.add(new JLabel("Sexo: "));
-		pRotulos.add(new JLabel("Academia: ")); //Filial da academia que o trainer pertence?
-	
+
+		pai = (TreinoConsulta)framePai;
+		cnx = conexao;
+		tl = new TreinoLogic(cnx);
+
+		pRotulos = new JPanel(new GridLayout(4,1,5,5));
+		pRotulos.add(new JLabel("Codigo"));
+		pRotulos.add(new JLabel("Vencimento"));
+		pRotulos.add(new JLabel("Cliente"));
+		pRotulos.add(new JLabel("Exercicio"));
+
 		tfCodigo = new JTextField();
-		tfNome = new JTextField();
-		tfDataNasc = new JTextField();
-		tfSexo = new JTextField();
-		tfAcademia = new JTextField();
+		tfVencimento = new JTextField();
+		tfCliente = new JTextField();
+		tfExercicio = new JTextField();
 		
-		pCampos = new JPanel(new GridLayout(7,1,5,5));
+		pCampos = new JPanel(new GridLayout(4,1,5,5));
 		pCampos.add(tfCodigo);
-		pCampos.add(tfNome);
-		pCampos.add(tfDataNasc);
-		pCampos.add(tfSexo);
-		pCampos.add(tfAcademia);
+		pCampos.add(tfVencimento);
+		pCampos.add(tfCliente);
+		pCampos.add(tfExercicio);
 		
 		pControles = new JPanel(new BorderLayout(5,5));
 		pControles.add(pRotulos, BorderLayout.WEST);
 		pControles.add(pCampos);
-		
+
 		btConfirmar = new JButton(actConfirmar);
 		btCancelar = new JButton(actCancelar);
 		
-		pOperacoes = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
+		try {
+			lblImagem = new JLabel(new ImageIcon(ImageIO.read(new File("src/Cadastro.png"))));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+		pOperacoes = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		pOperacoes.add(btConfirmar);
 		pOperacoes.add(btCancelar);
-		
-		this.add(pControles);
-		this.add(pOperacoes, BorderLayout.SOUTH);
-		
-		this.pack();
-		
+
+		add(pControles);
+		add(pOperacoes, BorderLayout.SOUTH);	
+		add(lblImagem, BorderLayout.EAST);
+
+		pack();
+
 	} //Fim do mÃ©todo construtor
-	
+
 	class AcaoConfirmar extends AbstractAction{
 
 		AcaoConfirmar(){
@@ -98,44 +108,32 @@ public class TrainerCadastro extends JFrame{
 					new ImageIcon(imagesPath+"general/Save24.gif"));
 
 		}
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
-
+			Date vencimento = null;
 			String strAtualize = "";
-			int codigo;
-			String nome;
-			Date dataNasc = null;
-			String sexo;
-			int academia;
-			
-
-			codigo = Integer.parseInt(tfCodigo.getText());
-			nome = tfNome.getText();
 			try {
-				dataNasc = (Date) form.parse(tfDataNasc.getText());
+				vencimento = form.parse(tfVencimento.getText());
 			} catch (ParseException e2) {
 				e2.printStackTrace();
 			}
-			sexo = (tfSexo.getText());                             //Em TrainerLogic sexo tÃ¡ como String 
-			academia = Integer.parseInt(tfAcademia.getText());     //e academia como int
 			
-
-			try {			
-
+			try {
 				switch (acao) {
 				case INCLUSAO:
-					tl.addTrainer(codigo, nome, dataNasc, sexo, academia);
+					 		   
+					tl.addTreino(Integer.parseInt(tfCodigo.getText()), vencimento, Integer.parseInt(tfCliente.getText()), Integer.parseInt(tfExercicio.getText()));
 					break;
 				case EDICAO:
-					tl.updTrainer(codigo, nome, dataNasc, sexo, academia);
+					tl.updTreino(Integer.parseInt(tfCodigo.getText()), vencimento, Integer.parseInt(tfCliente.getText()), Integer.parseInt(tfExercicio.getText()));
 					break;
 				case EXCLUSAO:
-					tl.delTrainer(codigo, nome, dataNasc, sexo, academia);				
+					tl.delTreino(Integer.parseInt(tfCodigo.getText()), vencimento, Integer.parseInt(tfCliente.getText()), Integer.parseInt(tfExercicio.getText()));
 					break;
 				}
 				limparCampos();
-				TrainerCadastro.this.setVisible(false);
+				TreinoCadastro.this.setVisible(false);
 				pai.setVisible(true);
 				pai.buscar();				
 			} catch (DataBaseGenericException | 
@@ -144,19 +142,19 @@ public class TrainerCadastro extends JFrame{
 					InvalidFieldException | 
 					EntityNotExistException e1) 
 			{
-				JOptionPane.showMessageDialog(TrainerCadastro.this, e1.getMessage(), 
+				JOptionPane.showMessageDialog(TreinoCadastro.this, e1.getMessage(), 
 						"Cadastro de Centro", JOptionPane.ERROR_MESSAGE);
 			}
-			
 		}
 	}
+
 	class AcaoCancelar extends AbstractAction{
 
 		AcaoCancelar(){
 			super("Cancelar");
 			putValue(MNEMONIC_KEY, KeyEvent.VK_L);
 			putValue(SHORT_DESCRIPTION, 
-					"Cancelar operaÃ§Ã£o!");
+					"Cancelar operacao!");
 			putValue(SMALL_ICON, 
 					new ImageIcon(imagesPath+"general/Stop24.gif"));
 
@@ -166,7 +164,7 @@ public class TrainerCadastro extends JFrame{
 		public void actionPerformed(ActionEvent e) {
 
 			limparCampos();
-			TrainerCadastro.this.setVisible(false);
+			TreinoCadastro.this.setVisible(false);
 			pai.setVisible(true);
 			pai.buscar();
 		}
@@ -176,13 +174,12 @@ public class TrainerCadastro extends JFrame{
 	public void incluir() {
 
 		acao = INCLUSAO;
-		setTitle("InclusÃ£o de Personal Trainer");
-
+		setTitle("Inclusao de Treino");
+		
 		tfCodigo.setEnabled(true);
-		tfNome.setEnabled(true);
-		tfDataNasc.setEnabled(true);
-		tfSexo.setEnabled(true);
-		tfAcademia.setEnabled(true);
+		tfVencimento.setEnabled(true);
+		tfCliente.setEnabled(true);
+		tfExercicio.setEnabled(true);
 		
 
 		limparCampos();
@@ -195,13 +192,12 @@ public class TrainerCadastro extends JFrame{
 	public void editar(int codigo) {
 
 		acao = EDICAO;
-		setTitle("EdiÃ§Ã£o de Personal Trainer");
+		setTitle("Edicao de Treino");
 
-		tfCodigo.setEnabled(true);
-		tfNome.setEnabled(true);
-		tfDataNasc.setEnabled(true);
-		tfSexo.setEnabled(true);
-		tfAcademia.setEnabled(true);
+		tfCodigo.setEnabled(false);
+		tfVencimento.setEnabled(true);
+		tfCliente.setEnabled(true);
+		tfExercicio.setEnabled(true);
 
 		carregarCampos(codigo);
 
@@ -214,59 +210,57 @@ public class TrainerCadastro extends JFrame{
 	public void excluir(int codigo) {
 
 		acao = EXCLUSAO;
-		setTitle("ExclusÃ£o de Personal Trainer");
+		setTitle("Exclusao de Treino");
 
-		tfCodigo.setEnabled(true);
-		tfNome.setEnabled(true);
-		tfDataNasc.setEnabled(true);
-		tfSexo.setEnabled(true);
-		tfAcademia.setEnabled(true);
+		tfCodigo.setEnabled(false);
+		tfVencimento.setEnabled(false);
+		tfCliente.setEnabled(false);
+		tfExercicio.setEnabled(false);
 
 		carregarCampos(codigo);
 
 		pai.setVisible(false);
-		setVisible(true);
+		this.setVisible(true);
 
 	}	
+
 
 	public void limparCampos() {
 
 		tfCodigo.setText("");
-		tfNome.setText("");
-		tfDataNasc.setText("");
-		tfSexo.setText("");
-		tfAcademia.setText("");
+		tfVencimento.setText("");
+		tfCliente.setText("");
+		tfExercicio.setText("");
 
 	}
 
 	public void carregarCampos(int codigo) {
 
-		Trainer p;
-				
+		Treino c;
+		Date vencimento = null;
+		
+//		try {
+//			vencimento = form.parse(tfVencimento.getText());
+//		} catch (ParseException e2) {
+//			e2.printStackTrace();
+//		}
+		
 		try {
-			p = tl.getTrainer(codigo);
-			
-			tfCodigo.setText(String.valueOf(p.getCodigo()));
-			tfNome.setText(p.getNome());
-			tfDataNasc.setText(String.valueOf(p.getDataNasc()));
-			tfSexo.setText(String.valueOf(p.getSexo()));
-			tfAcademia.setText(String.valueOf(p.getAcademia()));
-			//Em trainerlogic academia estÃ¡ como int e em trainer estÃ¡ como academia
-											 
-			
-
+			c = tl.getTreino(codigo);
+			tfCodigo.setText(Integer.toString(c.getCodigo()));
+			tfVencimento.setText(form.format(c.getVencimento()));
+			tfCliente.setText(c.getCliente().toString());
+			tfExercicio.setText(c.getExercicio().toString());
 		} catch (DataBaseGenericException | 
 				DataBaseNotConnectedException | 
 				EntityNotExistException e) 
 		{
 			JOptionPane.showMessageDialog(this, e.getMessage(), 
-					"Cadastro de Centro", JOptionPane.ERROR_MESSAGE);
+					"Cadastro de Treino", JOptionPane.ERROR_MESSAGE);
 		}
 
 
-
 	}
-	
-	
-	
+
+
 }
